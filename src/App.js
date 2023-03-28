cc0a6ef3206275ebeb4b0a5616c2e3e53ad16617
Valentyn <wolf.valentyn@gmail.com>
2023-03-28 09:53:15 +0300

    add function App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>PMG weather app</h1>
+      <div className="container">
+        <div className="left-panel">
+          <input id="search"></input>
+          <button>search</button>
+        </div>
+
+        <div className="right-panel">
+          <h1>City Name</h1>
+          <h2>Temperature in °C</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
