4fd2346679a9acc84a8a020e148ea5702d7746b6
Valentyn <wolf.valentyn@gmail.com>
2023-03-28 10:02:12 +0300

    add getResource

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,3 +1,11 @@
 class WeatherService {
-    
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status ${res.status}`);
+        }
+
+        return await res.json();
+    }
 }
